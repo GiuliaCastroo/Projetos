1c135252792494913eec5fbc8281a6f082eeadd9
Giulia Castro <maju.nogueir3.castr3@gmail.com>
2022-06-24 18:26:21 -0300

    Adicionando alterações de projetos

--- Marvel Project- Mapa Dev Week/js/index.js
+++ Marvel Project- Mapa Dev Week/js/index.js
@@ -1,26 +1,21 @@
+
 const personagens = document.querySelectorAll('.personagem')
 
-personagens.forEach((personagem) => {
-    personagem.addEventListener('mouseenter', () => {
+personagens.forEach(personagens => {
+  personagens.addEventListener('mouseenter', () => {
+    const personagemSelecionado = document.querySelector('.selecionado')
+    personagemSelecionado.classList.remove('selecionado')
+    personagens.classList.add('selecionado')
 
-        const idSelecionado = personagem.attributes.id.value;
 
-        if(idSelecionado === 'ultron') return;
+    
+    const idSelecionado = personagens.attributes.id.value;
+    const imagemJogador1 = document.getElementById('personagem-jogador-1');
+    imagemJogador1.src = `imagens/${idSelecionado}.png`
+    
+    const nomeJogador1 = document.getElementById('nome-personagem'); 
+    const nomeSelecionado = personagens.getAttribute('data-name');
+    nomeJogador1.innerHTML = nomeSelecionado;
+  })
 
-
-        const personagemSelecionado = document.querySelector('.selecionado')
-        personagemSelecionado.classList.remove('selecionado')
-        personagem.classList.add('selecionado')
-
-        
-
-        const  imagemJogador1= document.getElementById('personagem-jogador-1');
-        imagemJogador1.src = `./imagens/${idSelecionado}.png`;
-
-        const nomeJogador1 = document.getElementById('nome-jogador-1');
-        const nomeSelecionado = personagem.getAttribute('data-name');
-
-        nomeJogador1.innerHTML = nomeSelecionado;
-        
-    });
-});
+})
